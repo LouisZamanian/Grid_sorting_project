e058d3256b55f001bb6113250ab2514b778ff547
lzamanianabbasi-ensae <louis.zamanianabbasi@ensae.fr>
2024-02-07 17:28:17 +0000

    Ajout de la fonction de représentation de la grille avec matplotlib

--- swap_puzzle/grid.py
+++ swap_puzzle/grid.py
@@ -193,7 +193,20 @@ class Grid():
         return all_states
 
 
+    def representation(self):
+        fig, ax = plt.subplots()
+        ax.set_xlim(0, self.m)  #on créer les axes verticaux et horizontaux à la bonne taille
+        ax.set_ylim(0, self.n)
 
+        ax.set_xticks(range(self.n+1))
+        ax.set_yticks(range(self.m))
+        ax.set_xticklabels([]) #on supprime les graduations pour ne garder que la grille
+        ax.set_yticklabels([])
+        ax.grid(True, which='major', linestyle='-', linewidth=2, color='black') #on trace le cadrillage
+        for i in range(self.n):
+            for j in range(self.m):
+                ax.text(j+0.5,self.n-i-0.5, str(self.state[i][j]), fontsize=12) #on remplit la grille 
+        plt.show()
 
 
 
